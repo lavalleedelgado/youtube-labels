32eb8aa2309af9266743b030aa219133662d5b01
Patrick Lavallee <pld@cs.uchicago.edu>
2020-05-26 09:33:08 -0400

    Moved PLD pipeline work to subdirectory, just to keep things tidy.

--- pld/pipeline.py
+++ pld/pipeline.py
@@ -1,18 +1,16 @@
 import os
-from typing import Generator, List, Tuple
+from typing import Type, Generator, Tuple, List, Dict
 import csv
+import copy
 import re
 from collections import Counter
 import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
 
 
 # Identify constants for vocabulary operations.
 DELIMITERS = re.compile(r' |\\n|\|')
-PUNCTUATION = re.compile(r'[.:;,?!\"|#()-]|^\'|\'$')
-STOP_PATTERN = re.compile(r'http|www')
+PUNCTUATION = re.compile(r'[.:;,?!\"|#()-_•]|^\'|\'$')
+STOP_PATTERN = re.compile(r'http|www|^\s*$')
 STOP_WORDS = set(['the', 'a', 'and', 'or', 'of'])
 UNK = '<unk>'
 PAD = '<pad>'
@@ -36,14 +34,14 @@ def make_dataset(
     idx_text (tuple): column indices of text data to concatenate.
     idx_num (tuple): column indices of numeric data.
     binarize_label (str): positive class in the label.
-    **kwargs (dict): keyword arguments to pass to read_data().
+    **kwargs (dict): keyword arguments to pass to __read_data().
 
-    Return dataset np.ndarray.
+    Return dataset (np.ndarray).
     '''
     # Initialize a container for the dataset.
     dataset = []
     # Get a generator that reads the data from the file.
-    rows = read_data(filename, **kwargs)
+    rows = __read_data(filename, **kwargs)
     # Consider each row in the data.
     while True:
         row = next(rows, False)
@@ -73,7 +71,7 @@ def make_dataset(
     return np.vstack(dataset)
 
 
-def read_data(
+def __read_data(
     filename: str,
     delimiter: str = ',',
     has_header: bool = True
@@ -85,7 +83,7 @@ def read_data(
     delimiter (str): field separator, default is comma ','.
     has_header (bool): whether the data has a header row, default is True.
 
-    Return tokenized rows in the data (Generator of lists of strings).
+    Return tokenized rows in the data (generator of lists of strings).
     '''
     # Verify the file exists.
     if not os.path.exists(filename):
@@ -107,7 +105,7 @@ def split_data(data: np.ndarray, train: float, valid: float, test: float) -> Lis
 
     train, valid, test (float): proportional allocations of the data.
 
-    Return splits (list of np.ndarray objects).
+    Return splits (list of np.ndarray).
     '''
     # Ensure the proportions sum to one.
     assert sum([train, valid, test]) == 1
@@ -115,23 +113,23 @@ def split_data(data: np.ndarray, train: float, valid: float, test: float) -> Lis
     n = data.shape[0]
     idx = np.random.permutation(n)
     idx_train = idx[:int(n * train)]
-    idx_valid = idx[len(idx_train):len(idx_train) + int(n * valid)]
-    idx_testg = idx[len(idx_train) + len(idx_valid):]
+    idx_valid = idx[int(n * train):int(n * (train + valid))]
+    idx_testg = idx[int(n * (train + valid)):]
     # Return the subsets of data.
     return data[idx_train, ], data[idx_valid, ], data[idx_testg, ]
 
 
-def make_vocabulary(
+def make_vectorization(
     corpus: np.ndarray,
     n: int,
     tokenize_pattern: re.Pattern = DELIMITERS,
     clean_pattern: re.Pattern = PUNCTUATION,
     stop_words: set = STOP_WORDS,
-    drop_pattern: re.Pattern = STOP_PATTERN,
-) -> Tuple[List[tuple], dict]:
+    drop_pattern: re.Pattern = STOP_PATTERN
+) -> Tuple[List[np.ndarray], Dict[str, int]]:
     '''
-    Collect the vocabulary in the corpus and tally the frequency of each word.
-    Identify words on the delimiter and clean them with a regular expression.
+    Collect the vector representation of each sentence in the corpus from the
+    n most frequent words in its vocabulary.
 
     corpus (np.ndarray): n x 1 array of text to parse.
     n (int): size of the vocabulary chosen from most frequent words.
@@ -140,33 +138,58 @@ def make_vocabulary(
     stop_words (set): words to remove, i.e. articles, prepositions.
     drop_pattern (re.Pattern): regular expression for words to remove.
 
-    Return cleaned corpus, mapping of words to indices (list, dict).
+    Return the vectorized corpus and vocabulary (list, dict).
     '''
-    vocabulary = {}
+    # Make the vocabulary and clean the corpus.
+    corpus, vocab = __make_vocab(corpus, tokenize_pattern, clean_pattern)
+    # Drop stop words and keep only the n most frequent words.
+    vocab = __drop_words(vocab, n, stop_words, drop_pattern)
+    # Map words in the vocabulary to indices with entries for padding, unknown.
+    vocab = __make_index(vocab)
+    # Map each word in the corpus to its index in the vocabulary.
+    corpus = __numericize_words(corpus, vocab)
+    # Return the vectorized corpus and its vocabulary.
+    return corpus, vocab
+
+
+def __make_vocab(
+    corpus: np.ndarray,
+    tokenize_pattern: re.Pattern,
+    clean_pattern: re.Pattern
+) -> Tuple[np.ndarray, dict]:
+    '''
+    Collect the vocabulary in the corpus and tally the frequency of each word.
+    Identify words on the delimiter and clean them with a regular expression.
+
+    corpus (np.ndarray): n x 1 array of text to parse.
+    tokenize_pattern (re.Pattern): regular expression for delimiters.
+    clean_pattern (re.Pattern): regular expression for substrings to remove.
+
+    Return cleaned corpus, mapping of words to indices (np.ndarray, dict).
+    '''
+    vocab = {}
+    # Make a deep copy of the corpus because it arrives to scope by reference.
+    corpus = copy.deepcopy(corpus)
     # Consider each sentence in the corpus.
     for s in range(corpus.shape[0]):
-        sentence = tokenize_words(corpus[s], tokenize_pattern)
+        sentence = __tokenize_words(corpus[s], tokenize_pattern)
         # Consider each word in the sentence.
         for w in range(len(sentence)):
             # Clean this word.
-            word = clean_words(sentence[w], clean_pattern)
+            word = __clean_words(sentence[w], clean_pattern)
             # Update the frequency of this word in the vocabulary.
-            if word not in vocabulary:
-                vocabulary[word] = 0
-            vocabulary[word] += 1
+            if word not in vocab:
+                vocab[word] = 0
+            vocab[word] += 1
             # Update this word in the sentence.
             sentence[w] = word
         # Update this sentence in the corpus.
         corpus[s] = ' '.join(word for word in sentence if word != '')
-    # Drop stop words and keep only the n most frequent words.
-    vocabulary = drop_words(vocabulary, n, stop_words, drop_pattern)
-    # Map words in the vocabulary to indices with entries for padding, unknown.
-    vocabulary = make_index(vocabulary)
     # Return the updated corpus and the vocabulary.
-    return corpus, vocabulary
+    return corpus, vocab
 
 
-def tokenize_words(text: str, pattern: re.Pattern) -> List[str]:
+def __tokenize_words(text: str, pattern: re.Pattern) -> List[str]:
     '''
     Tokenize text on the delimiter.
 
@@ -178,24 +201,24 @@ def tokenize_words(text: str, pattern: re.Pattern) -> List[str]:
     return re.split(pattern, text)
 
 
-def clean_words(text: str, pattern: re.Pattern) -> str:
+def __clean_words(text: str, pattern: re.Pattern) -> str:
     '''
     Clean text of substrings.
 
     text (str): collection of characters to clean.
     pattern (re.Pattern): regular expression for substrings to remove.
 
-    Return cleaned text (string).
+    Return cleaned text (str).
     '''
-    return re.sub(pattern, '', text).lower()
+    return re.sub(pattern, '', text.lower())
 
 
-def drop_words(vocabulary: dict, n: int, stop_words: set, pattern: re.Pattern) -> set:
+def __drop_words(vocab: dict, n: int, stop_words: set, pattern: re.Pattern) -> set:
     '''
     Remove words from the vocabulary that are among the stop words or match a
     regular expression and keep only the n most frequent.
 
-    vocabulary (dict): mapping of words to frequencies.
+    vocab (dict): mapping of words to frequencies.
     n (int): the number of most frequent words to keep.
     stop_words (set): words to remove.
     pattern (re.Pattern): regular expression for words to remove.
@@ -203,62 +226,80 @@ def drop_words(vocabulary: dict, n: int, stop_words: set, pattern: re.Pattern) -
     Return vocabulary (set).
     '''
     # Find words that match the stop pattern.
-    for word in vocabulary:
+    for word in vocab:
         if re.search(pattern, word):
             stop_words.add(word)
     # Remove stop words from the vocabulary.
     for word in stop_words:
-        del vocabulary[word]
+        del vocab[word]
     # Return the most frequent words.
-    return {word for word, _ in Counter(vocabulary).most_common(n)}
+    return {word for word, _ in Counter(vocab).most_common(n)}
 
 
-def make_index(vocabulary: set) -> dict:
+def __make_index(vocab: set) -> Dict[str, int]:
     '''
     Collect indices for each word in the vocabulary. Include tokens for unknown
     words and padding.
 
-    vocabulary (set): words in the vocabulary.
+    vocab (set): words in the vocabulary.
 
     Return mapping of words to indices (dict).
     '''
-    vocabulary.update([UNK, PAD])
-    return {word: i for i, word in enumerate(vocabulary)}
+    vocab.update([UNK, PAD])
+    return {word: i for i, word in enumerate(vocab)}
 
 
-def make_vectorization(
+def __numericize_words(
     corpus: np.ndarray,
-    vocabulary: dict,
-    tokenize_pattern: re.Pattern = DELIMITERS
-) -> np.ndarray:
+    vocab: dict,
+    tokenize_pattern: re.Pattern = DELIMITERS,
+) -> List[np.ndarray]:
     '''
-    Represent the corpus as an n x m matrix that encodes each of n sentences
-    as a vector with the frequencies of each of m words in the vocabulary.
+    Represent each word in the corpus with its index in the vocabulary.
 
-    corpus (np.ndarray): n x 1 array of text to vectorize.
-    vocabulary (dict): mapping of m words to indices.
+    corpus (np.ndarray): n x 1 array of text to parse.
+    vocab (dict): mapping of words to indices.
     tokenize_pattern (re.Pattern): regular expression for delimiters.
 
-    Return n x m matrix representation of the corpus (np.ndarray).
+    Return the numericized corpus (list).
     '''
-    # Initialize an n x m array for the data.
-    n = corpus.shape[0]
-    m = len(vocabulary)
-    data = np.zeros(shape=(n, m))
-    # Get the length of the longest sentence.
-    k = max(len(sentence) for sentence in corpus)
-    # Get the index of the unknown word.
-    idx_unk = vocabulary[UNK]
+    data = []
     # Consider each sentence in the corpus.
-    for s in range(n):
-        sentence = tokenize_words(corpus[s], tokenize_pattern)
-        # Pad this sentence to the length of the longest.
-        sentence += [PAD] * (k - len(sentence))
-        # Map the index of each word to its frequency in the sentence.
-        words = Counter(vocabulary.get(word, idx_unk) for word in sentence)
-        # Update the matrix representation.
-        for w, frequency in words.items():
-            data[s, w] = frequency
-    # Return the vectorized corpus.
+    for s in range(corpus.shape[0]):
+        words = __tokenize_words(corpus[s], tokenize_pattern)
+        # Map each word in the sentence to its index in the vocabulary.
+        sentence = np.array([vocab.get(word, UNK) for word in words])
+        # Add this sentence to the data.
+        data.append(sentence)
+    # Return the numericized corpus.
     return data
+
+
+def make_batches(
+    labels: np.ndarray,
+    corpus: List[np.ndarray],
+    default: int,
+    n: int
+) -> Generator[Tuple[np.ndarray, np.ndarray], None, None]:
+    '''
+    Generate label, data pairs of the data of length n. Pad sentences with a
+    default value such that each in the same batch is of the same length.
+
+    Return paired labels and data (generator of tuples of np.ndarray).
+    '''
+    # Verify the size of the labels and corpus match.
+    assert labels.shape[0] == corpus.shape[0]
+    # Calculate the number of batches to generate.
+    batches = (labels.shape[0] + n - 1) // n
+    # Generate each batch.
+    for i in range(batches):
+        idx = i * n
+        # Initialize this batch with the length of its longest sentence.
+        m = max(corpus[idx + j].shape[0] for j in range(n))
+        batch = np.full(shape=(n, m), fill_value=default)
+        # Load each sentence in this batch.
+        for j in range(n):
+            batch[j, :corpus[idx + j].shape[0]] = corpus[idx + j]
+        # Yield this batch.
+        yield labels[idx:idx + n], batch
 
